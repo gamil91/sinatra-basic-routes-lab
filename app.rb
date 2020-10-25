243b79da21df2d2574c3032c378f280464670c84
gamil91 <alexis_gamil99@yahoo.com.com>
2020-10-25 16:33:17 -0700

    commit tree f4086d74de3bc318a76fe3e924cea868329e06cd

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/name' do
+        "My name is Alex"
+    end
+
+    get '/hometown' do
+        "My hometown is Los Angeles"
+    end
+
+    get '/favorite-song' do
+        "My favorite song is 'You Were Good To Me' by Jeremy Zucker"
+    end
+
 end
